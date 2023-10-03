5db47c3d24891f76e57a187f2c5f03a50f3a5f61
takumi kosaka <takumikosaka@takuminoMacBook-Pro.local>
2023-10-03 14:31:50 +0900

    feat: modal設計練習

--- composable/example4/useErrorModal.ts
+++ composable/example4/useErrorModal.ts
@@ -7,25 +7,25 @@ type ExportProps = {
   };
 };
 
-export const useErrorModal = (): ExportProps => {
-  const errorCodeMessageMapping: Record<
-    ErrorModalProps["errorCode"],
-    { title: string; body: string }
-  > = {
-    "404": {
-      title: "404 Not Found",
-      body: "ページが見つかりませんでした。",
-    },
-    "500": {
-      title: "500 Internal Server Error",
-      body: "サーバーでエラーが発生しました。",
-    },
-    other: {
-      title: "エラー",
-      body: "エラーが発生しました。",
-    },
-  };
+const errorCodeMessageMapping: Record<
+  ErrorModalProps["errorCode"],
+  { title: string; body: string }
+> = {
+  "404": {
+    title: "404 Not Found",
+    body: "ページが見つかりませんでした。",
+  },
+  "500": {
+    title: "500 Internal Server Error",
+    body: "サーバーでエラーが発生しました。",
+  },
+  other: {
+    title: "エラー",
+    body: "エラーが発生しました。",
+  },
+};
 
+export const useErrorModal = (): ExportProps => {
   const getErrorModalText = (errorCode: ErrorModalProps["errorCode"]) => {
     return {
       title: errorCodeMessageMapping[errorCode].title,
